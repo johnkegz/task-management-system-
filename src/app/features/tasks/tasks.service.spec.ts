5c6228fc7ecb3bc6fd7931bdaa4dcb28925c716e
johnkegz <johnkal24@gmail.com>
2024-06-12 13:57:31 +0300

    add tests for tasks dasboard and service

--- src/app/features/tasks/tasks.service.spec.ts
+++ src/app/features/tasks/tasks.service.spec.ts
@@ -1,16 +1,63 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TaskService } from './tasks.service';
+import { Task } from './tasks.model';
 
-import { TasksService } from './tasks.service';
-
-describe('TasksService', () => {
-  let service: TasksService;
+describe('TaskService', () => {
+  let service: TaskService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TasksService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TaskService]
+    });
+    service = TestBed.inject(TaskService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
   });
+
+
+
+  it('should create a new task', () => {
+    const newTask: Task = { id: 3, title: 'Task 3', description: 'Description 3', user: 'User 3', status: 'Pending' };
+
+    service.createTask(newTask).subscribe(task => {
+      expect(task).toEqual(newTask);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/tasks');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(newTask);
+    req.flush(newTask);
+  });
+
+  it('should update an existing task', () => {
+    const updatedTask: Task = { id: 3, title: 'Updated Task', description: 'Updated Description', user: 'User 1', status: 'Completed' };
+
+    service.updateTask(1, updatedTask).subscribe(task => {
+      expect(task).toEqual(updatedTask);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/tasks/1');
+    expect(req.request.method).toBe('PUT');
+    expect(req.request.body).toEqual(updatedTask);
+    req.flush(updatedTask);
+  });
+
+    it('should retrieve all tasks', () => {
+  const tasks: Task[] = [{ id: 1, title: 'Updated Task', description: 'Updated Description', user: 'User 1', status: 'Completed' }];
+
+  service.getTasks().subscribe((responseTasks: Task[]) => {
+    expect(responseTasks.length).toBe(1);
+    expect(responseTasks).toEqual(tasks);
+  });
+
+  const req = httpMock.expectOne('http://localhost:3000/tasks');
+  expect(req.request.method).toBe('GET');
+  req.flush(tasks);
+});
 });
